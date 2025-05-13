7cf83c5a86c178076f5115b3ebaefe070d1c7dbc
Josue Limo <josuelimomunoz@gmail.com>
2025-05-13 14:28:15 -0300

    Agregue clase Gestion de turno con registro de los turnos

--- sistemaGestionTurno/src/Modelo/Turno.java
+++ sistemaGestionTurno/src/Modelo/Turno.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Modelo;
 
 /**
@@ -61,10 +58,5 @@ public class Turno {//clase de turno. Describir sus atributos y funciones.
     public String toString() {
         return "Turno{" + "fechaTurno=" + fechaTurno + ", tipoNota=" + tipoNota + ", codigoSeg=" + codigoSeg + ", alum=" + alum + '}';
     }
-    
-    
-    
-    
-    
-    
+      
 }
